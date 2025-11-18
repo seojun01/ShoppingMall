e452ad19499cfba78134265f31e230d48e0e3609
seojun01 <parkseojoon@naver.com>
2025-11-18 21:51:37 +0900

    ReactRouterDom2

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,51 @@ import Navbar from "react-bootstrap/Navbar";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import data from "./data";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 import { ItemContainer, DetailComponent } from "./components";
+
+const EventPage = () => {
+  return (
+    <>
+      <div className="eventPage">
+        <h4>오늘의 이벤트</h4>
+        <Outlet></Outlet>
+      </div>
+    </>
+  );
+};
 
 function App() {
   let [shoes] = useState(data);
+  let navigate = useNavigate();
 
   return (
     <div className="App">
       {/* navbar */}
       <Navbar bg="dark" data-bs-theme="dark" className="navBar">
         <Container>
-          <Navbar.Brand href="/">ShoeShop</Navbar.Brand>
+          <Navbar.Brand
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            ShoeShop
+          </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/detail">Detail</Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/detail");
+              }}
+            >
+              Detail
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -53,7 +83,16 @@ function App() {
             </>
           }
         />
+        {/* Detail 페이지 */}
         <Route path="/detail" element={<DetailComponent />} />
+
+        <Route path="/event" element={<EventPage />}>
+          <Route path="one" element={<p>첫 주문시 양배추즙 서비스</p>} />
+          <Route path="two" element={<p>생일기념 쿠폰받기</p>} />
+        </Route>
+
+        {/* 404 페이지 */}
+        <Route path="*" element={<div>404 Not Found</div>} />
       </Routes>
     </div>
   );
